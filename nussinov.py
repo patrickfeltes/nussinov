d77bbc2c7c34cf9fd7c16e055680e8a0842c3ff4
Michael Kokkines <32113818+mkokkines@users.noreply.github.com>
2020-12-01 20:00:34 -0600

    generate all possible answers and store in files

--- nussinov.py
+++ nussinov.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import sys
+import copy
 
 def nussinov(rna_sequence, base_pairings):
     n = len(rna_sequence)
@@ -33,37 +34,98 @@ def nussinov(rna_sequence, base_pairings):
     return s
 
 def traceback(s):
-    stack = []
-    traceback_list = []
-    paren_list = []
+    stacks = []
+    traceback_lists = []
+    paren_lists = []
+    results = []
 
-    stack.append((0, len(s) - 1))
-    traceback_list.append((0, len(s) - 1))
+    initial_stack = []
+    initial_stack.append((0, len(s) - 1))
+    stacks.append(initial_stack)
+    initial_traceback = []
+    initial_traceback.append((0, len(s) - 1))
+    traceback_lists.append(initial_stack)
+    initial_paren_list = []
+    paren_lists.append(initial_paren_list)
 
-    while len(stack) != 0:
-        i, j = stack.pop()
-        if i >= j:
-            continue
-        elif s[i + 1][j] == s[i][j]:
-            stack.append((i + 1, j))
-            traceback_list.append((i + 1, j))
-        elif s[i][j - 1] == s[i][j]:
-            stack.append((i, j - 1))
-            traceback_list.append((i, j - 1))
-        elif s[i + 1][j - 1] + 1 == s[i][j]:
-            stack.append((i + 1, j - 1))
-            traceback_list.append((i + 1, j - 1))
-            paren_list.append((i, j))
-        else:
+    while len(stacks) != 0:
+        prev_stacks = copy.deepcopy(stacks)
+        prev_traceback_lists = copy.deepcopy(traceback_lists)
+        prev_paren_lists = copy.deepcopy(paren_lists)
+
+        stacks = []
+        traceback_lists = []
+        paren_lists = []
+
+        for i in range(len(prev_stacks)):
+            stack = prev_stacks[i]
+            traceback_list = prev_traceback_lists[i]
+            paren_list = prev_paren_lists[i]
+            
+            if len(stack) == 0:
+                results.append((traceback_list, paren_list))
+                continue
+            
+            i, j = stack.pop()
+            if i >= j:
+                stacks.append(stack)
+                traceback_lists.append(traceback_list)
+                paren_lists.append(paren_list)
+                continue
+            
+            if s[i + 1][j] == s[i][j]:
+                stack_c = copy.deepcopy(stack)
+                traceback_c = copy.deepcopy(traceback_list)
+                paren_list_c = copy.deepcopy(paren_list)
+                
+                stack_c.append((i + 1, j))
+                traceback_c.append((i + 1, j))
+               
+                stacks.append(stack_c)
+                traceback_lists.append(traceback_c)
+                paren_lists.append(paren_list_c)
+
+            if s[i][j - 1] == s[i][j]:
+                stack_c = copy.deepcopy(stack)
+                traceback_c = copy.deepcopy(traceback_list)
+                paren_list_c = copy.deepcopy(paren_list)
+
+                stack_c.append((i, j - 1))
+                traceback_c.append((i, j - 1))
+
+                stacks.append(stack_c)
+                traceback_lists.append(traceback_c)
+                paren_lists.append(paren_list_c)
+
+            if s[i + 1][j - 1] + 1 == s[i][j]:
+                stack_c = copy.deepcopy(stack)
+                traceback_c = copy.deepcopy(traceback_list)
+                paren_list_c = copy.deepcopy(paren_list)
+
+                stack_c.append((i + 1, j - 1))
+                traceback_c.append((i + 1, j - 1))
+                paren_list_c.append((i, j))
+                                      
+                stacks.append(stack_c)
+                traceback_lists.append(traceback_c)
+                paren_lists.append(paren_list_c)
+
             for k in range(i + 1, j):
                 if s[i][k] + s[k + 1][j] == s[i][j]:
-                    stack.append((k + 1, j))
-                    stack.append((i, k))
-                    traceback_list.append((k + 1, j))
-                    traceback_list.append((i, k))
-                    break
+                    stack_c = copy.deepcopy(stack)
+                    traceback_c = copy.deepcopy(traceback_list)
+                    paren_list_c = copy.deepcopy(paren_list)
 
-    return traceback_list, paren_list
+                    stack_c.append((k + 1, j))
+                    stack_c.append((i, k))
+                    traceback_c.append((k + 1, j))
+                    traceback_c.append((i, k))
+
+                    stacks.append(stack_c)
+                    traceback_lists.append(traceback_c)
+                    paren_lists.append(paren_list_c)
+                
+    return results
 
 def dot_parentheses_notation(s, traceback_list):
     string = ['.'] * len(s)
@@ -72,7 +134,7 @@ def dot_parentheses_notation(s, traceback_list):
         string[j] = ')'
     return ''.join(string)
 
-def generate_visualization(s, traceback_list, rna_sequence, dot_parentheses_string):
+def generate_visualization(s, traceback_list, rna_sequence, dot_parentheses_string, num, output_prefix):
     columns = tuple(rna_sequence)
     rows = list(rna_sequence)
 
@@ -92,7 +154,8 @@ def generate_visualization(s, traceback_list, rna_sequence, dot_parentheses_stri
     plt.title(dot_parentheses_string)
     ax.axis('tight')
     ax.axis('off')
-    plt.show()
+    plt.savefig(output_prefix + str(num) + ".png")
+    plt.clf()
 
 if __name__ == "__main__":
     base_pairings = {
@@ -103,7 +166,14 @@ if __name__ == "__main__":
     }
 
     seq = sys.argv[1]
+    output_prefix = sys.argv[1]
     table = nussinov(seq, base_pairings)
-    traceback_list, paren_list = traceback(table)
-    dot_parentheses_string = dot_parentheses_notation(table, paren_list)
-    generate_visualization(table, traceback_list, seq, dot_parentheses_string)
+    results = traceback(table)
+    seen_dot_paren = []
+
+    for i in range(len(results)):
+        (traceback_list, paren_list) = results[i]
+        dot_parentheses_string = dot_parentheses_notation(table, paren_list)
+        if dot_parentheses_string not in seen_dot_paren:
+            seen_dot_paren.append(dot_parentheses_string)
+            generate_visualization(table, traceback_list, seq, dot_parentheses_string, i, output_prefix)
