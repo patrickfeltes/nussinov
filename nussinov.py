0f45718457f82daa43c1271bf18dc8fdde7ebbe2
Michael Kokkines <32113818+mkokkines@users.noreply.github.com>
2020-12-01 19:06:00 -0600

    cleaned up traceback

--- nussinov.py
+++ nussinov.py
@@ -44,16 +44,20 @@ def traceback(s):
             continue
         elif s[i + 1][j] == s[i][j]:
             stack.append((i + 1, j))
+            traceback_list.append((i + 1, j))
         elif s[i][j - 1] == s[i][j]:
             stack.append((i, j - 1))
+            traceback_list.append((i, j - 1))
         elif s[i + 1][j - 1] + 1 == s[i][j]:
-            traceback_list.append((i, j))
             stack.append((i + 1, j - 1))
+            traceback_list.append((i + 1, j - 1))
         else:
             for k in range(i + 1, j):
                 if s[i][k] + s[k + 1][j] == s[i][j]:
                     stack.append((k + 1, j))
                     stack.append((i, k))
+                    traceback_list.append((k + 1, j))
+                    traceback_list.append((i, k))
                     break
 
     return traceback_list
@@ -70,15 +74,18 @@ def generate_visualization(s, traceback_list, rna_sequence):
     rows = list(rna_sequence)
 
     colors = []
+    numbers = []
     for i in range(len(rna_sequence)):
         colors.append((["w"] * len(rna_sequence)))
-    
+        numbers.append(([""] * len(rna_sequence)))
+
     # Now, set the cells that should be colored as green
     for (r, c) in traceback_list:
         colors[r][c] = "#42f55d"
+        numbers[r][c] = str(s[r][c])
 
     _, ax = plt.subplots()
-    ax.table(loc='center', cellColours=colors, colLabels=columns, rowLabels=rows, cellText=s)
+    ax.table(loc='center', cellColours=colors, colLabels=columns, rowLabels=rows, cellText=numbers)
     ax.axis('tight')
     ax.axis('off')
     plt.show()
@@ -94,5 +101,5 @@ if __name__ == "__main__":
     seq = 'GCACGACG'
     table = nussinov(seq, base_pairings)
     traceback_list = traceback(table)
-    dot_parentheses_string = dot_parentheses_notation(table, traceback_list))
+    dot_parentheses_string = dot_parentheses_notation(table, traceback_list)
     generate_visualization(table, traceback_list, seq)
