3fa45c038caaaff0880d7ddf1c0795ebe3afdd30
patrickfeltes <pj6444@gmail.com>
2020-12-18 19:20:26 -0600

    Add sequence input form:

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import NussinovTable from './components/NussinovTable';
 
+let handleFormSubmit = (event) => {
+  event.preventDefault();
+  console.log(event.target.sequence.value);
+}
+
 function App() {
   return (
-    <NussinovTable />
+    <div>
+      <form onSubmit={handleFormSubmit}>
+          <label>
+            RNA Sequence: <input type="text" name="sequence" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      <NussinovTable />
+    </div>
   );
 }
 
